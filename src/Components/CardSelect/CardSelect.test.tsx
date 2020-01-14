fe6c70d965dfbb760ca4f37f6245b33a38b5a001
Kong Chae Won <chaewonkong@gmail.com>
2020-01-14 16:17:12 +0900

    style: add img-card with width of 300px

--- src/Components/CardSelect/CardSelect.test.tsx
+++ src/Components/CardSelect/CardSelect.test.tsx
@@ -14,5 +14,13 @@ describe("CardSelect", () => {
     it("renders correctly", () => {
       expect(cardSelect.find("Image").exists()).toBe(true);
     });
+
+    it("has `wrapper` class", () => {
+      expect(cardSelect.find(".wrapper").exists()).toBe(true);
+    });
+
+    it("has `img-card` class", () => {
+      expect(cardSelect.find(".img-card").exists()).toBe(true);
+    });
   });
 });
--- src/Components/CardSelect/index.tsx
+++ src/Components/CardSelect/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Image } from "react-bootstrap";
+import "./cardSelect.scss";
 
 const CardSelect: React.FC = () => {
   return (
-    <div>
+    <div className="wrapper">
       <Image
+        className="img-card"
         src="https://images.unsplash.com/photo-1516100882582-96c3a05fe590?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
         rounded
+        fluid
       />
     </div>
   );
