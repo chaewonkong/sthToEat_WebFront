d72f426d0ba58fc30e97f76508841b448e3a614d
Kong Chae Won <chaewonkong@gmail.com>
2020-01-22 15:07:55 +0900

    fix: change function name to async

--- src/store/sagas/saga.ts
+++ src/store/sagas/saga.ts
@@ -1,21 +1,28 @@
 import { put, takeLatest, all } from "redux-saga/effects";
 import { DishAction } from "../actions/dish";
 import * as constants from "../actions/constants";
+import { IDishPayload } from "../shared-interfaces";
 
-function* likeDish(payload) {
-  yield put({ type: constants.LIKE_DISH_ASYNC, payload });
+export function* likeDishAsync() {
+  // const products = yield api.fetch('/products')
+  yield put({ type: constants.LIKE_DISH_ASYNC, payload: {} });
 }
 
-function* dislikeDish(payload) {
-  yield put({ type: constants.DISLIKE_DISH_ASYNC, payload });
+export function* dislikeDishAsync() {
+  // const products = yield api.fetch('/products')
+  yield put({ type: constants.DISLIKE_DISH_ASYNC, payload: {} });
+}
+
+function* a() {
+  yield put({ type: "", value: 1 });
 }
 
 function* watchLikeDish() {
-  yield takeLatest(constants.LIKE_DISH, likeDish);
+  yield takeLatest(constants.LIKE_DISH, likeDishAsync);
 }
 
 function* watchDislikeDish() {
-  yield takeLatest(constants.DISLIKE_DISH, dislikeDish);
+  yield takeLatest(constants.DISLIKE_DISH, dislikeDishAsync);
 }
 
 export default function* rootSaga() {
